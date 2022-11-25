1808c88f012c0c7df66c7a764ee9c3e5831b6ec9
Nguyễn Quang Khải <khai@myspa.vn>
2022-11-25 17:24:55 +0700

    [UPDATE] fix loading component

--- src/pages/partials/Loading.tsx
+++ src/pages/partials/Loading.tsx
@@ -1,17 +1,13 @@
-import React from "react"
+import React from "react";
+import '../../styles/loading.css';
+
 export default function index() {
     return (
-        <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-            width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
-            <path fill="#000" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
-                <animateTransform attributeType="xml"
-                    attributeName="transform"
-                    type="rotate"
-                    from="0 25 25"
-                    to="360 25 25"
-                    dur="0.6s"
-                    repeatCount="indefinite" />
-            </path>
-        </svg>
+        <div className="mainLoad">
+            <svg width="200" height="200" viewBox="0 0 40 60">
+                <polygon className="triangle" fill="none" stroke="#fff" stroke-width="1" points="16,1 32,32 1,32"/>
+                <text className="loading" x="0" y="45" fill="#fff" >Loading...</text>
+            </svg>
+        </div>
     )
 }
